5028755a61952befb0d310b586b812c92924fb2a
Royce Joseph <78349539+AnneRoyce-married@users.noreply.github.com>
2024-07-17 13:27:54 +0100

    CompanyInfoController details

--- CompanyInfoApi/Controllers/CompanyInfoController.cs
+++ CompanyInfoApi/Controllers/CompanyInfoController.cs
@@ -1,25 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CompanyInfoApi.Models;
+using CompanyInfoApi.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using CompanyInfoApi.Data;
-using CompanyInfoApi.Models;
+using System.Threading.Tasks;
+
 
 namespace CompanyInfoApi.Controllers
 {
+    [ApiController]
+[Route("api/[controller]")]
+[Authorize] ;
+
     public class CompanyInfoController : Controller
     {
-        private readonly CompanyInfoApiContext _context;
+        private readonly ICompanyInfoService _companyInfoService;
+        private readonly ICompaniesHouseApiService _companiesHouseApiService;
 
-        public CompanyInfoController(CompanyInfoApiContext context)
+        public CompanyInfoController(ICompanyInfoService companyInfoService, ICompaniesHouseApiService companiesHouseApiService)
         {
-            _context = context;
+            _companyInfoService = companyInfoService;
+            _companiesHouseApiService = companiesHouseApiService;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> AddCompanyInfo([FromBody] CompanyInfo model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-        
+            var existingCompanyInfo = await _companiesHouseApiService.GetCompanyInfoAsync(model.CompanyNumber);
+            if (existingCompanyInfo != null)
+            {
+                existingCompanyInfo.RetrievedAt = DateTime.UtcNow;
+                await _companyInfoService.CreateCompanyInfoAsync(existingCompanyInfo);
+                return Ok(existingCompanyInfo);
+            }
+
+            return NotFound("Company not found");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCompanyInfo(int id)
+        {
+            var companyInfo = await _companiesHouseApiService.GetCompanyInfoAsync(id.ToString());
+            if (companyInfo == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyInfo);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanyInfos()
+        {
+            var companyInfos = await _companyInfoService.GetAllCompanyInfosAsync();
+            return Ok(companyInfos);
+        }
     }
+
+}
 }
